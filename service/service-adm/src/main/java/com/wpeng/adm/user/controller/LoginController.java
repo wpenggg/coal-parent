9b529aa5959b808d4cadef95f9e83db335488a5e
wangpeng <wangpeng@wangpeng.com>
2020-12-21 17:32:58 +0800

    登录 静态请求 添加

--- service/service-adm/src/main/java/com/wpeng/adm/user/controller/LoginController.java
+++ service/service-adm/src/main/java/com/wpeng/adm/user/controller/LoginController.java
@@ -1,29 +1,29 @@
-//package com.wpeng.adm.user.controller;
-//
-//import com.wpeng.commonutils.api.APICODE;
-//import org.springframework.web.bind.annotation.*;
-//
-///**
-// * @author wpeng
-// * @version 1.0
-// * @date 2020/12/14 17:18
-// */
-//
-//
-//    @RestController
-//    @RequestMapping("/adm/user")
-//    @CrossOrigin
-//    public class LoginController {
-//
-//        @PostMapping("login")
-//        public APICODE login() {
-//            return APICODE.OK().data("token", "admin");
-//        }
-//
-//        @GetMapping("info")
-//        public APICODE info() {
-//            return APICODE.OK().data("roles", "[admin]").data("name", "admin").data("avatar", "http://img0.imgtn.bdimg.com/it/u=1782959667,617309577&fm=26&gp=0.jpg");
-//        }
-//
-//    }
-//}
+package com.wpeng.adm.user.controller;
+
+import com.wpeng.commonutils.api.APICODE;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author wpeng
+ * @version 1.0
+ * @date 2020/12/14 17:18
+ */
+
+
+@RestController
+@RequestMapping("/adm/user")
+@CrossOrigin
+public class LoginController {
+    //
+    @PostMapping("login")
+    public APICODE login() {
+        return APICODE.OK().dataMap("token", "admin");
+    }
+
+    @GetMapping("info")
+    public APICODE info() {
+        return APICODE.OK().dataMap("roles", "[admin]").dataMap("name", "admin").dataMap("avatar", "http://img0.imgtn.bdimg.com/it/u=1782959667,617309577&fm=26&gp=0.jpg");
+    }
+
+}
+
